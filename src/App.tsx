36cc15f59c93124614283bda53df09d75366c122
赵单 <zhaodan4@yusys.com.cn>
2021-03-24 15:39:16 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link,
+//     Redirect
+// } from "react-router-dom";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav';
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags />
+                        </Route>
+                        <Route path="/money">
+                            <Money />
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics />
+                        </Route>
+                        <Redirect exact from="/" to="/money"></Redirect>
+                        <Route path="*">
+                            <NoMatch />
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav />
+            </Wrapper>
+        </Router>
+    );
+}
+function NoMatch() {
+    return (
+        <h2>没有找到匹配页面</h2>
+    )
+}
+function Statistics() {
+    return <h2>统计页</h2>;
+}
+
+function Tags() {
+    return <h2>标签页</h2>;
+}
+
+function Money() {
+    return <h2>记账页</h2>;
 }
 
 export default App;
