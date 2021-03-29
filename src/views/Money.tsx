67089590ed7c9fa67a2afc15868b6bcb52e09ce2
赵单 <zhaodan4@yusys.com.cn>
2021-03-29 10:55:36 +0800

    完成 noteSection的功能以及input的受控及非受控写法

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import styled from 'styled-components';
 import {TagsSection} from './money/TagsSection';
-import {NotesSection} from './money/NotesSection';
+import {NoteSection} from './money/NoteSection';
 import {CategorySection} from './money/CategorySection';
 import {NumberPadSection} from './money/NumberPadSection';
 const MyLayout = styled(Layout)`
@@ -14,12 +14,8 @@ function Money() {
         <MyLayout>
             <TagsSection>
             </TagsSection>
-            <NotesSection>
-                <label>
-                    <span>备注</span>
-                    <input type="text" placeholder="在这里添加备注"/>
-                </label>
-            </NotesSection>
+            <NoteSection>
+            </NoteSection>
             <CategorySection>
                 <ul>
                     <li className="selected">支出</li>
---
+++ src/views/money/NoteSection.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import React, {useRef, useState} from 'react';
+
+const Wrapper = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
+const NoteSection: React.FC = () => {
+    const [note,setNote] = useState('');
+    //以下是input的非受控写法
+    const refInput = useRef<HTMLInputElement>(null)
+    const onBlur = ()=> {
+        if(refInput.current!== null){
+            //console.log(refInput.current.value);
+            setNote(refInput.current.value);
+        }
+    }
+    return(
+        <Wrapper>
+            <label>
+                <span>备注</span>
+                {/*以下是input的受控写法*/}
+                {/*<input type="text" placeholder="在这里添加备注" value={note} onChange={(e)=>{setNote(e.target.value)}}/>*/}
+                {/*以下是input的非受控写法*/}
+                <input type="text" ref={refInput} placeholder="在这里添加备注" defaultValue={note} onBlur={onBlur} />
+            </label>
+        </Wrapper>
+    )
+}
+export { NoteSection }
